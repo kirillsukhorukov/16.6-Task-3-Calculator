4b8e280545b6f3f0262eb722db9a256307886c7b
kirillsukhorukov <sukhorukov_kirill@mail.ru>
2023-03-16 09:49:17 +0700

    initial

---
+++ 16.6-Task-3-Calculator.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "-----Calculator-----" << endl;
+    //Ввод строки с действием
+    string buffer;
+    //Цикл работы калькулятора
+    while (true)
+    {
+        cout << "Enter the operation (+-*/) with two numbers: ";
+        cin >> buffer;
+        //Организация выхода из цикла
+        if (buffer == "exit") break;
+        //Разделение строки на составляющие
+        istringstream bufferStream(buffer);
+        double a, b;
+        char operation;
+        bufferStream >> a >> operation >> b;
+        //Обработка команды
+        if (operation == '+') cout << a << operation << b << '=' << a + b << endl;
+        else if (operation == '-') cout << a << operation << b << '=' << a - b << endl;
+        else if (operation == '*') cout << a << operation << b << '=' << a * b << endl;
+        else if (operation == '/') cout << a << operation << b << '=' << a / b << endl;
+        else cout << "Incorrect operation!" << endl;
+    }
+   
+    return 0;
+}
